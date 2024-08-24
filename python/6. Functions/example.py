2b485efad8c7a2172420248778bae7557f5bb1fd
ScienceZoneUK <sciencezoneuk@gmail.com>
2024-08-24 15:11:10 +0100

    Create example.py

---
+++ python/6. Functions/example.py
@@ -0,0 +1,17 @@
+def function():
+  print("This is a function!")
+
+def addition(a, b):
+  answer = a + b
+  return answer
+
+function()
+function()
+
+number1 = 10
+number2 = 5
+addition(number1, number2)
+
+number1 = 6
+number2 = 91083923
+addition(number1, number2)
