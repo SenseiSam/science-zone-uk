b40bda7c2d58a0f5f42f1f19ecee7f93a05d3187
ScienceZoneUK <sciencezoneuk@gmail.com>
2024-08-24 15:26:16 +0100

    Update example.py

--- python/2.1 Data Types/example.py
+++ python/2.1 Data Types/example.py
@@ -1,38 +1,46 @@
-#python has different data types that can serve different puporses
-'''Some of the common data types are whole numbers which (also know as intergers)
-decimals are known as floats, as string of characters e.g 'bike', '42hudfh9' are
-know as strings and True/False statements are known as boolean data types
+# Python has different ways of storing data, known as data types that can serve different purposes
+'''
+Some of the common data types are:
+- Whole numbers which called integers in Python.
+- Decimals/Fractions which are known as floats or reals.
+- Strings of characters, or text. e.g 'bike', '42hudfh9'
+- True/False statements are known as Booleans
+
+Here is a brief demo showcasing the assignment of the 4 data types
+listed above into a variable
 '''
 
-'''here is a brief demo showcasing the assignment of the  data types 6
-listed above into a variable'''
-
-
-#string
+# string
 fruit = "apple"
 dessert = 'cake'
 
-#Notice that two different types of quotes were when assign this string variable
-#the string is the only data type that requires quotes when assigning it.
+# Notice that two different types of quotes were used when assigning these string variables
+# The string is the only data type that requires quotes when assigning it.
 
-#integer
-num1 =12
+# integer
+num1 = 12
 
-#float
-num2 =13.34
+# float
+num2 = 13.34
 
-#boolean
-state1=True
-state2=False
+# boolean
+state1 = True
+state2 = False
 
-# if you are not sure of a data type you always use the type() function and print
+# If you are not sure of a data type you can always use the type() function. Pass this into a print() to see the variable's data type
 #your answer
+
+datatype = type(num1)
+print(datatype)
 
 print(type(num1))
 
-#Chanlenge what data type will num3 below be?
-
+# Challenge: what data type will num3 below be?
+num1 = 4
+num2 = 7
 num3 = num1 + num2
+
+# Extend: What data type will num3 be if num1 or num2 were floats?
 
 
 
