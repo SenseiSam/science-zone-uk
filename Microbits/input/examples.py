4a313a9db23f15cea2939f1d75472bb575583e02
Dan Rogers <130596844+danrog729@users.noreply.github.com>
2024-09-07 14:46:36 +0100

    Update examples.py

--- Microbits/input/examples.py
+++ Microbits/input/examples.py
@@ -1,6 +1,8 @@
-'''The microbit had two main buttons A and B there are different ways that you
-this buttons can be used to send commands to the microbit here you will be
-learning how to make a number/string show on when a button is pressed'''
+'''
+The microbit has two buttons: A and B. 
+These buttons can be used to send commands to the microbit. 
+Here you will be learning how to make a number/string show when the buttons are pressed.
+'''
 
 #the code below is a function that lets you insert the commands you want to happen
 #when a button is pressed
