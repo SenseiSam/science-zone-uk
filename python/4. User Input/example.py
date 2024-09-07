68be2d27177d7db11f85db87febc0b2ee716731c
Dan Rogers <130596844+danrog729@users.noreply.github.com>
2024-09-07 14:23:11 +0100

    Update example.py

--- python/4. User Input/example.py
+++ python/4. User Input/example.py
@@ -8,3 +8,14 @@ integer = int(integer)
 print(integer + 10)
 
 # You'll  notice that if you enter something that isn't a whole number into the above code, it will crash. We will learn how to deal with this later.
+
+name = input("What is your name? ")
+
+string = input("What is your age? ")
+age = int(string)
+
+rating = int(input("Rate how you're feeling on a scale of 1-10: "))
+
+print("Your name is " + name)
+print("You are " + str(age) + " years old.")
+print("You are feeling like a " + rating + " out of 10.")
