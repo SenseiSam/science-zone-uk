f0f2f7d786a20d3bc6973c780e0dec3b907ac841
Dan Rogers <130596844+danrog729@users.noreply.github.com>
2024-09-07 14:27:06 +0100

    Update example.py

--- python/7. Functions & Procedures/example.py
+++ python/7. Functions & Procedures/example.py
@@ -1,17 +1,18 @@
-def function():
-  print("This is a function!")
+def procedure():
+  print("This is a procedure! Can you tell me the difference between a procedure and a function?")
 
 def addition(a, b):
   answer = a + b
+  print(number1 + " + " + number2 + " = " + result)
   return answer
 
-function()
-function()
+procedure()
+procedure()
+# Code can be repeated easily using functions and procedures.
 
 number1 = 10
 number2 = 5
-addition(number1, number2)
+result = addition(number1, number2)
 
-number1 = 6
 number2 = 91083923
-addition(number1, number2)
+result = addition(result, number2)
