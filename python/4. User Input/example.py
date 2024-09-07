d0c285472297bcafae423b5a5f946930fbd6f119
Dan Rogers <130596844+danrog729@users.noreply.github.com>
2024-09-07 14:20:17 +0100

    Create example.py

---
+++ python/4. User Input/example.py
@@ -0,0 +1,10 @@
+userInput = input("Enter anything here: ")
+print(userInput)
+
+# user input will always be a string. You need to cast this to other data types if you want to use it.
+
+integer = input("Enter a whole number: ")
+integer = int(integer)
+print(integer + 10)
+
+# You'll  notice that if you enter something that isn't a whole number into the above code, it will crash. We will learn how to deal with this later.
