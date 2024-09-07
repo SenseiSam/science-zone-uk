0ad57f0a92f82a61c0e2774eb217b5b375b3887d
Dan Rogers <130596844+danrog729@users.noreply.github.com>
2024-09-07 14:27:35 +0100

    Update example.py

--- python/7. Functions & Procedures/example.py
+++ python/7. Functions & Procedures/example.py
@@ -2,6 +2,7 @@ def procedure():
   print("This is a procedure! Can you tell me the difference between a procedure and a function?")
 
 def addition(a, b):
+  # This is a function. It returns the answer after the numbers have been added.
   answer = a + b
   print(number1 + " + " + number2 + " = " + result)
   return answer
