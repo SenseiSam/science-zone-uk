926ab32afd386f0a1647fb28587a410956073dfa
Dan Rogers <130596844+danrog729@users.noreply.github.com>
2024-09-07 14:16:12 +0100

    Update example.py

--- python/3. Data Types/example.py
+++ python/3. Data Types/example.py
@@ -1,39 +1,44 @@
-# Python has different ways of storing data, known as data types that can serve different purposes
 '''
+Python has different ways of storing data, known as data types. This is so the computer understands what the data means and what can be done with it.
+
 Some of the common data types are:
-- Whole numbers which called integers in Python.
+- Whole numbers which are called integers in Python.
 - Decimals/Fractions which are known as floats or reals.
-- Strings of characters, or text. e.g 'bike', '42hudfh9'
 - True/False statements are known as Booleans
+- Strings of characters, or text. e.g 'bike', '42hudfh9' (we usually just call these strings)
 
-Here is a brief demo showcasing the assignment of the 4 data types
-listed above into a variable
+Here is a brief demo showcasing the assignment of the 4 data types listed above into a variable
 '''
-
-# string
-fruit = "apple"
-dessert = 'cake'
-
-# Notice that two different types of quotes were used when assigning these string variables
-# The string is the only data type that requires quotes when assigning it.
 
 # integer
 num1 = 12
+answerToEverything = 42
 
 # float
 num2 = 13.34
+pi = 3.141592653589
 
 # boolean
 state1 = True
 state2 = False
 
-# If you are not sure of a data type you can always use the type() function. Pass this into a print() to see the variable's data type
-#your answer
+# string
+fruit = "apple"
+dessert = 'cake'
 
+'''
+Notice that two different types of quotes were used when assigning these string variables. These both do the same thing in Python.
+The string is the only data type that requires quotes when assigning it.
+
+If you are not sure of a data type you can always use the type() function. Pass this into a print() to see the variable's data type your answer
+'''
+
+num1 = 12
 datatype = type(num1)
 print(datatype)
 
-print(type(num1))
+string = "Hello world!"
+print(type(string))
 
 # Challenge: what data type will num3 below be?
 num1 = 4
