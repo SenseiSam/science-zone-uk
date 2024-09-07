832996a9306de96ed9d805f5a1aa7ca15471b244
Dan Rogers <130596844+danrog729@users.noreply.github.com>
2024-09-07 14:44:45 +0100

    Update examples.py

--- Microbits/basics/examples.py
+++ Microbits/basics/examples.py
@@ -1,13 +1,12 @@
-'''Welcome to the microbit course
-    here you will be learning some really
-    fun projects
-    to send code to you microbit it is important that you connect to
-    to the microbit makecode editor here is a
-    link to access it https://makecode.microbit.org/#editor
 '''
-#Firstly we will be working with the LED on the microbit
-#the micro bit has a number of LED lights infront of it you will be
-# displaying some numbers and text soon on the microbit
+Welcome to the Microbit course!
+Here you will be learning to make some really fun projects using the BBC Microbit!
+To send code to your microbit you need to connect to the Microbit Makecode editor.
+Here is a link to access it: https://makecode.microbit.org/#editor
+
+Firstly, we will be working with the LED on the microbit. The microbit has a number of LED lights on its front. 
+You will be displaying some numbers and text using these LEDs.
+'''
 
 #how to display a number on a microbit in python
 basic.show_number(0)
